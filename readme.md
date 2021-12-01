c12126cb6ac855dd4294a9b469d4aa72f9caeb70
chia-fec <chiaravisca@flexecharge.com>
2021-12-01 10:49:03 +0100

    first commit (not working yet)

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Frontend of Cinemaxxx
+This project will serve as a proof of concept for the organization Cinema-xx.
+
+## Backend/API: 
+https://github.com/VingardiumGloriosa/Cinema-XXX
---
+++ router.js
@@ -0,0 +1,19 @@
+import renderMain from "./pages/main/main.js";
+import renderAbout from "./pages/about/about.js";
+
+export default function () {
+  window.router = new Navigo("/", { hash: true });
+
+  router
+    .on({
+      "/": () => {
+        // call updatePageLinks to let navigo handle the links
+        // when new links have been inserted into the dom
+        renderMain().then(router.updatePageLinks);
+      },
+      about: () => {
+        renderAbout();
+      },
+    })
+    .resolve();
+}
