2c8b2b926d0df92ebed596ed8a1fd74e9c4f591b
chia <chiaravisca@outlook.com>
2021-12-02 13:12:11 +0100

    Base layer ready

--- settings.js
+++ settings.js
@@ -4,7 +4,9 @@ export default function () {
     window.location.host.indexOf("localhost") != -1;
 
   const localApiUrl = "http://localhost:9090";
-  const prodApiUrl = //TODO: add our api host
+  const prodApiUrl = "http://54.221.49.14:9090/"
+
+  // SWAGGER: http://54.221.49.14:9090/swagger-ui.html#/
 
   // when fetching data from the api we need to know what the url is
   // It is different if you are developing locally or have the site deployed
