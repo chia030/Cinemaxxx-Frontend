bfd8b7036f32f168c9b677e05556cbdd0fd934e9
Glória <63670594+VingardiumGloriosa@users.noreply.github.com>
2021-12-04 15:21:24 +0000

    not much but its honest work

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,4 +1,20 @@
-export default () => {
+  fetch("http://54.221.49.14:9090/api/movies")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log("kill me");
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    var div = document.getElementsByClassName("content");
+    div.innerHTML += "suck my dick"; 
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+  export default () => {
     const content = document.querySelector(".content");
   
     return fetch("./pages/movies/movies.html")
@@ -6,4 +22,4 @@ export default () => {
       .then((moviesHtml) => {
         content.innerHTML = moviesHtml;
       });
-  };
+  }; 
