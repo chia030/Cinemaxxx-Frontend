b02ba97a8eb6888578db9fcb901cbea812a44773
TheSamuelBartek <82447376+TheSamuelBartek@users.noreply.github.com>
2021-12-05 21:35:21 +0100

    my

--- pages/timetable/timetable.js
+++ pages/timetable/timetable.js
@@ -1,46 +1,67 @@
-document.addEventListener("DOMContentLoaded", init);
 
-function init() {
-  document.getElementById("btnSearch").addEventListener("click", ev => {
+
+const getTimetable = () => { 
     ev.preventDefault(); //to stop the page reload
     let dateFrom = document.getElementById("date-from").value;
     let dateTo = document.getElementById("date-to").value;
 
     // need to change for server when its up and running
-    let url = `http://localhost:8080/api/screenings/get?date1=${dateFrom}&date2=${dateTo}`;
-    
-    fetch(url)
-      .then(response => response.json())
-      .then(content => {
-        //  data, pagination, meta
-        console.log(content.data);
-        console.log("META", content.meta);
-        for(let i = 0; i <= 9; i++){
+    const Http = new XMLHttpRequest();
+    let url = `http://54.221.49.14:9090/api/screenings/get?date1=${dateFrom}&date2=${dateTo}`;
+
+    Http.open("GET", url);
+   Http.send(); //async request
+
+   const timetableParagraph = document.querySelector("#timetable");
+   timetableParagraph.innerHTML ="";
+
+   // when something changes ->
+   Http.onreadystatechange = (e) => {
+
+    const timetableArray = JSON.parse(Http.response);
+    console.log(timetableArray);
+ 
+  timetableArray.forEach(screening => {
+    timetableParagraph.innerHTML += screening.movie.title + " " + screening.time + "<br>";
+    });
+  }
+} 
+  
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(content => {
+//         //  data, pagination, meta
+//         console.log(content.data);
+//         console.log("META", content.meta);
+//         for(let i = 0; i <= 9; i++){
        
-        let fig = document.createElement("figure");
-        let paragraph = document.createElement("p");
+//         let fig = document.createElement("figure");
+//         let paragraph = document.createElement("p");
 
-        paragraph.textContent = content.data[i].movie.title + " " + content.data[i].time;
+//         paragraph.textContent = content.data[i].movie.title + " " + content.data[i].time;
 
-        fig.appendChild(paragraph);
-        let out = document.querySelector(".out");
-        out.insertAdjacentElement("afterbegin", fig); //so the new images are on the top not the bottom
-        }
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  });
-}
-
+//         fig.appendChild(paragraph);
+//         let out = document.querySelector(".out");
+//         out.insertAdjacentElement("afterbegin", fig); //so the new images are on the top not the bottom
+//         }
+//       })
+//       .catch(err => {
+//         console.error(err);
+//       });
+//   });
+// }
 
 
 export default () => {
-  const content = document.querySelector(".content");
+    const content = document.querySelector(".content");
+  
+    return fetch("./pages/timetable/timetable.html")
+      .then((response) => response.text())
+      .then((timetableHtml) => {
+        content.innerHTML = timetableHtml;
 
-  fetch("./pages/timetable/timetable.html")
-    .then((response) => response.text())
-    .then((aboutHtml) => {
-      content.innerHTML = timetableHtml;
-    });
-};
+        //setting onclick to getMovies()
+        const displayScheduleButton = document.querySelector("#btnSearch");
+        displayScheduleButton.onclick = getTimetable();   
+      });
+  };
