f0a01d65402d6e4bc21c4cc8cc48a3b138396207
Glória <63670594+VingardiumGloriosa@users.noreply.github.com>
2021-12-08 19:35:42 +0000

    *shoots self*

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,11 +1,48 @@
+//get movie posters on load
+function getMoviePosters(){ 
+  console.log("please");
+
+  const Http = new XMLHttpRequest();
+  const url="http://54.221.49.14:9090/api/movies";
+
+  Http.open("GET", url);
+  Http.send(); //async request
+
+  //const moviesParagraph = document.querySelector(".movies");
+  //moviesParagraph.innerHTML = "";
+
+  var img = document.createElement("img");
+  var src = document.getElementsByClassName("movie");
+  
+  // when something changes ->
+  Http.onreadystatechange = (e) => {
+  const movieArray = JSON.parse(Http.response);
+    //console.log(Http.response);  
+    
+
+    movieArray.forEach(movie => {
+      var str = movie.poster;
+      img.src = str.replace(/\s/g, '');
+      console.log(img.src);
+      //src.appendChild(img);
+    });
+
+  }
+
+}
+
+function randomly(){
+  console.log("randomly");
+}
 
 export default () => {
-
   const content = document.querySelector(".content");
 
   return fetch("./pages/main/main.html")
     .then((response) => response.text())
     .then((mainHtml) => {
-      content.innerHTML = mainHtml;      
+      content.innerHTML = mainHtml;  
+      console.log("fuck me");  
+      getMoviePosters();  
     });
 };
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -22,41 +22,14 @@ const getMovies = () => {
 
   }
 
-
-  // I DIDN'T KNOW HOW TO WORK WITH THIS SORRYYYYYYYYY
-  // fetch("http://54.221.49.14:9090/api/movies")
-  // .then((response) => {
-  //   if (response.ok) {
-  //     return response.json();
-  //   } else {
-  //     console.log("kill me");
-  //     throw new Error("NETWORK RESPONSE ERROR");
-  //   }
-  // })
-  // // .then(data => {
-  // //   console.log(data);
-  // //   var div = document.getElementsByClassName("content");
-  // //   div.innerHTML += "suck my dick"; 
-  // // })
-  // .catch((error) => console.error("FETCH ERROR:", error));
-
 }
 
-
   export default () => {
-    
     const content = document.querySelector(".content");
-    
-    
+  
     return fetch("./pages/movies/movies.html")
       .then((response) => response.text())
       .then((moviesHtml) => {
-
         content.innerHTML = moviesHtml;
-
-        //setting onclick to getMovies()
-        const displayMoviesButton = document.querySelector("#display-movies-button");
-        displayMoviesButton.onclick = getMovies;     
-        
       });
   }; 
