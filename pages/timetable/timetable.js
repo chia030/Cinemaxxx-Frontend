6d70f0cf4dcebfc56b69f69a670a8e46ee1b31cc
TheSamuelBartek <82447376+TheSamuelBartek@users.noreply.github.com>
2021-12-08 19:51:44 +0100

    New timtable

--- pages/timetable/timetable.js
+++ pages/timetable/timetable.js
@@ -1,46 +1,39 @@
-document.addEventListener("DOMContentLoaded", init);
-
-function init() {
-  document.getElementById("btnSearch").addEventListener("click", ev => {
-    ev.preventDefault(); //to stop the page reload
-    let dateFrom = document.getElementById("date-from").value;
-    let dateTo = document.getElementById("date-to").value;
-
-    // need to change for server when its up and running
-    let url = `http://localhost:8080/api/screenings/get?date1=${dateFrom}&date2=${dateTo}`;
-    
-    fetch(url)
-      .then(response => response.json())
-      .then(content => {
-        //  data, pagination, meta
-        console.log(content.data);
-        console.log("META", content.meta);
-        for(let i = 0; i <= 9; i++){
-       
-        let fig = document.createElement("figure");
-        let paragraph = document.createElement("p");
-
-        paragraph.textContent = content.data[i].movie.title + " " + content.data[i].time;
-
-        fig.appendChild(paragraph);
-        let out = document.querySelector(".out");
-        out.insertAdjacentElement("afterbegin", fig); //so the new images are on the top not the bottom
-        }
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  });
-}
-
-
 
 export default () => {
   const content = document.querySelector(".content");
 
-  fetch("./pages/timetable/timetable.html")
+  return fetch("./pages/timetable/timetable.html")
     .then((response) => response.text())
-    .then((aboutHtml) => {
+    .then((timetableHtml) => {
       content.innerHTML = timetableHtml;
+
+      form.addEventListener("click", (event) => { 
+  
+        event.preventDefault();
+        const dateFrom = form.querySelector("input.date-from").value;
+        const dateTo = form.querySelector("input.date-to").value;
+      
+        // need to change for server when its up and running
+        const Http = new XMLHttpRequest();
+        let url = `http://54.221.49.14:9090/api/screenings/get?date1=${dateFrom}&date2=${dateTo}`;
+      
+        Http.open("GET", url);
+       Http.send(); //async request
+      
+       const timetableParagraph = document.querySelector("#timetable");
+       timetableParagraph.innerHTML ="";
+      
+       // when something changes ->
+       Http.onreadystatechange = (e) => {
+      
+        const timetableArray = JSON.parse(Http.response);
+        console.log(timetableArray);
+      
+      timetableArray.forEach(screening => {
+        timetableParagraph.innerHTML += screening.movie.title + " " + screening.time + "<br>";
+        });
+      }
+      
+  
     });
-};
+})};
