37d7ea6e74cfe4aa625176a0e79f3601057be9bc
TheSamuelBartek <82447376+TheSamuelBartek@users.noreply.github.com>
2021-12-11 15:48:16 +0100

    ok

--- router.js
+++ router.js
@@ -5,32 +5,14 @@ import renderTickets from "./pages/tickets/tickets.js";
 import renderTimetable from "./pages/timetable/timetable.js";
 
 export default function () {
-  const router = new Navigo("/", { hash: true });
+
+  window.router = new Navigo("/", {hash: true});
 
   router
     .on({ 
       "/": () => {
         // call updatePageLinks to let navigo handle the links
         // when new links have been inserted into the dom
-<<<<<<< HEAD
-        renderMain().then(router.updatePageLinks);
-      },
-      about: () => {
-        renderAbout();
-      },
-      movies: () => {
-        renderMovies();
-      },
-      "/tickets/:id/": ({ data, params }) => {
-        renderTickets(data.id);
-      },
-      timetable: () => {
-        renderTimetable().then(router.updatePageLinks);
-      }
-    })
-=======
-
-
       renderMain().then(router.updatePageLinks);
     },
     about: () => {
@@ -49,6 +31,5 @@ export default function () {
         renderTimetable().then(router.updatePageLinks);
      }
   })
->>>>>>> 6fcd5c5a6325ad10265b8eb3ad116af68483b338
     .resolve();
 }
