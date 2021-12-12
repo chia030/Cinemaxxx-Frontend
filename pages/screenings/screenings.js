76e3e7dd491a01f277166454f531083c7852947f
chia <chiaravisca@outlook.com>
2021-12-12 23:17:19 +0100

    :'(

--- pages/screenings/screenings.js
+++ pages/screenings/screenings.js
@@ -1,64 +1,75 @@
 //fetches ALL screenings on load, can be filtered with the 'Search' button
 const fetchScreenings = (url) => {
 
-    return fetch(url)
-           .then((response) => response.json())
-           .then((responseArray) => {
-            const output = document.querySelector('.screenings')
-            const screeningDiv = document.createElement("div");
-  
-              responseArray.forEach(screening => {
+  return fetch(url)
+         .then((response) => response.json())
+         .then((responseArray) => {
+          document.querySelector('.screenings').textContent = '';
 
-                const imgSrc = screening.movie.poster.replace(/\s/g, '');
+          const output = document.querySelector('.screenings')
+
+            responseArray.forEach(screening => {
+              
+              const screeningInfo = document.createElement("div");
+              const movieTitleExists = document.getElementById(screening.movie.title);
+              const imgSrc = screening.movie.poster.replace(/\s/g, '');
+              
+
+              const link = `<h1>${screening.movie.title}</h1><br>
+                            <img src="${imgSrc}">`;
+                    
 
 
-                const title = `<h1>${screening.movie.title}</h1><br>
-                              <img src="${imgSrc}">`;
+             // screeningInfo.innerHTML += link;
 
+              const info = document.createElement("ul");
+              info.innerHTML = `<a href='/#/tickets/${screening.screeningId}' data-navigo>
+              <li class="date">Date: ${screening.date}</li>
+                                <li class="time">Time: ${screening.time}</li>
+                                <li class="hall">Hall-ID: ${screening.hall.hallId}</li></a>`;
+              
 
-                const link = document.createElement("ul");
-                link.innerHTML = `<a href='/#/tickets/${screening.screeningId}' data-navigo>
-                                  <li class="screening-id">#${screening.screeningId}</li>
-                                  <li class="date">Date: ${screening.date}</li>
-                                  <li class="time">Time: ${screening.time}</li>
-                                  <li class="hall">Hall-ID: ${screening.hall.hallId}</li>
-                                  <li class="cinema">Cinema: ${screening.cinema.name}, ${screening.cinema.address}</li>
-                                  </a>`;
+              if(movieTitleExists){
+                
+                movieTitleExists.appendChild(info);
+            }else{
+              screeningInfo.innerHTML += link;
+              screeningInfo.id = screening.movie.title;
+              screeningInfo.appendChild(info);
+              output.insertAdjacentElement("afterbegin", screeningInfo);
+              }
 
-                screeningDiv.innerHTML += title;
-                screeningDiv.appendChild(link);
+            });
 
-              });
+          //  output.innerHTML = screeningInfo.outerHTML;
 
-              output.innerHTML = screeningDiv.outerHTML;
-
-  })};
+})};
    
-  export default () => {
+export default () => {
 
-      const content = document.querySelector(".content");
-    
-      return fetch("./pages/screenings/screenings.html")
-        .then((response) => response.text())
-        .then((screeningsHtml) => {
-          content.innerHTML = screeningsHtml;
-  
-          const allScreeningsUrl = `http://54.158.180.212:9090/api/screenings/get`;
-  
-          fetchScreenings(allScreeningsUrl);
-          
-          // when 'Filter' is pressed ->
-          document.querySelector("#btn-search").onclick = (e) => {
+  const content = document.querySelector(".content");
 
-            e.preventDefault();
+  return fetch("./pages/screenings/screenings.html")
+    .then((response) => response.text())
+    .then((screeningsHtml) => {
+      content.innerHTML = screeningsHtml;
 
-            let dateFrom = document.getElementById("date-from").value;
-            let dateTo = document.getElementById("date-to").value;
-            
-            const screeningByDateUrl = `http://54.158.180.212:9090/api/screenings/get?date1=${dateFrom}&date2=${dateTo}`;
-  
-            return fetchScreenings(screeningByDateUrl);
-        }
-      });
-  
-    };
+      const allScreeningsUrl = `http://54.158.180.212:9090/api/screenings/get`;
+
+      fetchScreenings(allScreeningsUrl);
+      
+      // when 'Search' is pressed ->
+      document.querySelector("#btn-search").onclick = (e) => {
+
+        e.preventDefault();
+
+        let dateFrom = document.getElementById("date-from").value;
+        let dateTo = document.getElementById("date-to").value;
+        
+        const screeningByDateUrl = `http://54.158.180.212:9090/api/screenings/get?date1=${dateFrom}&date2=${dateTo}`;
+
+        return fetchScreenings(screeningByDateUrl);
+    }
+  });
+
+};
