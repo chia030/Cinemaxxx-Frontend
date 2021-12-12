e892c4b1cbd972e6a630b0dc98b5ecb6e7bcab8c
Glória <63670594+VingardiumGloriosa@users.noreply.github.com>
2021-12-12 19:55:08 +0000

    css fix, requires js fix

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,30 +1,25 @@
 //get movie posters on load
 async function loadMoviePosters() {
 
-  let response = await fetch('http://54.158.180.212:9090/api/movies');
-  const movieArray = await response.json();
   const moviesParagraph = document.querySelector(".movies-container");
-  
-/*
-  movieArray.forEach(movie => {
-    var div = document.createElement("div");
-    div.className="movie";  
-    var img = document.createElement("img");
-    var str = movie.poster;
-    img.src = img.src = str.replace(/\s/g, '');
-    div.appendChild(img);
-    moviesParagraph.appendChild(div );
-  });
-*/
-  response = await fetch('http://54.158.180.212:9090/api/screenings/get');
+
+  let response = await fetch('http://54.158.180.212:9090/api/screenings/get');
   const screeningArray = await response.json();
-  
+  let screeningPosterArr = [];  
+
   screeningArray.forEach(screening => {
+    screeningPosterArr.push(screening.movie.poster);
+  });
+
+  let uniquePosters = [...new Set(screeningPosterArr)];
+
+  uniquePosters.forEach(screening => {
     var div = document.createElement("div");
     div.className="movie";  
     var img = document.createElement("img");
-    var str = screening.movie.poster;
-    img.src = img.src = str.replace(/\s/g, '');
+    img.src = screening;
+    img.classList.add("modal-open");
+    img.setAttribute("data-id","demo-modal");
     div.appendChild(img);
     moviesParagraph.appendChild(div );
   });
