4fe65ddd18c668e5976b1ab8547528ca10f9fb1b
Glória <63670594+VingardiumGloriosa@users.noreply.github.com>
2021-12-12 15:20:24 +0000

    some popup workin thingies

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -21,7 +21,79 @@ const getMovies = () => {
     });
 
   }
-}*/
+}
+
+console.log(document.getElementsByClassName("movie-poster"));
+window.onclick = e => {
+  let img = e.target;
+  console.log(img.src);
+} */
+
+let modal;
+document.addEventListener("click", (e) => {
+  if (e.target.className === "modal-open") {
+    let img = e.target;
+    console.log(img.src);
+    modal = document.getElementById(e.target.dataset.id);
+    openModal(modal);
+  } else if (e.target.className === "modal-close") {
+    closeModal(modal);
+  } else {
+    return;
+  }
+});
+
+async function getMovieInfo() {
+  let response = await fetch("http://54.158.180.212:9090/api/movies/");
+  const moviesssss = await response.json();
+
+  response = await fetch("http://54.158.180.212:9090/api/movies/id/tt0295297");
+  const movie = await response.json();
+
+  const movieTitle = document.getElementsByClassName("movie-title")[0];
+  const movieDesc = document.getElementsByClassName("movie-desc")[0];
+
+  var p = document.createElement('p');
+  p.className="movie-desc"; 
+  var title = document.createElement('p');
+  title.className="movie-title"; 
+
+  title.innerHTML = movie.title;
+  p.innerHTML = "Description : "+movie.description +"<br> Actors : "+movie.actors+"<br> Genre : "+movie.genre+"<br> Length : "+movie.length+"<br> Rating : "+movie.rating;
+
+  movieDesc.appendChild(p);  
+  movieTitle.appendChild(title);
+}
+
+
+const openModal = (modal) => {
+  document.body.style.overflow = "hidden";
+  modal.setAttribute("open", "true");
+  document.addEventListener("keydown", escClose);
+  let overlay = document.createElement("div");
+  overlay.id = "modal-overlay";
+  document.body.appendChild(overlay);
+  getMovieInfo();
+};
+
+const closeModal = (modal) => {
+  document.body.style.overflow = "auto";
+  modal.removeAttribute("open");
+  document.removeEventListener("keydown", escClose);
+  document.body.removeChild(document.getElementById("modal-overlay"));
+  let toRemove = document.getElementsByClassName("movie-desc")[1];
+  toRemove.remove();
+  toRemove = document.getElementsByClassName("movie-title")[1];
+  toRemove.remove();
+};
+
+const escClose = (e) => {
+  if (e.keyCode == 27) {
+    closeModal();
+  }
+};
+
+//coolio modal part done
 
 //get movie posters on load
 async function loadCurrentMoviePosters() {
@@ -37,14 +109,14 @@ async function loadCurrentMoviePosters() {
   });
 
   let uniquePosters = [...new Set(screeningPosterArr)];
-  console.log(uniquePosters);
 
   uniquePosters.forEach(screening => {
-    console.log(screening);
     var div = document.createElement("div");
     div.className="movie";  
     var img = document.createElement("img");
     img.src = screening;
+    img.classList.add("modal-open");
+    img.setAttribute("data-id","demo-modal");
     div.appendChild(img);
     moviesParagraph.appendChild(div );
   });
@@ -63,6 +135,8 @@ async function loadAllMoviePosters() {
     var img = document.createElement("img");
     var str = movie.poster;
     img.src = img.src = str.replace(/\s/g, '');
+    img.classList.add("modal-open");
+    img.setAttribute("data-id","demo-modal");
     div.appendChild(img);
     moviesParagraph.appendChild(div );
   });
